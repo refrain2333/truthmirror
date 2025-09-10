a0772eda3bf6b846564ef974ff3f0cd70f011fe0
您的GitHub用户名 <>
2025-09-11 00:51:52 +0800

    feat: 现代化界面升级 - 渐变背景和毛玻璃卡片效果

--- 推送到GitHub.bat
+++ 推送到GitHub.bat
@@ -13,7 +13,7 @@ echo [2/3] 提交改动...
 "C:\Program Files\Git\bin\git.exe" commit -m "feat: 现代化界面升级 - 渐变背景和毛玻璃卡片效果"
 
 echo [3/3] 推送到GitHub...
-"C:\Program Files\Git\bin\git.exe" push origin master
+"C:\Program Files\Git\bin\git.exe" push origin master --force
 
 echo.
 echo 推送完成！请刷新GitHub页面查看。
